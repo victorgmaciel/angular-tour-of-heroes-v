5bc33d69cb45626a10580aa87d052592be919522
Victor! <victor88@uw.edu>
2022-06-22 13:01:22 -0700

    angular project

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './/heroes/hero';
+
+export const HEROES: Hero[] = [
+    {id: 12, name: 'Dr.Doom' },
+    {id: 13, name: 'Magneto' },
+    {id: 14, name: 'Onslaught' },
+    {id: 15, name: 'Dr. Xavier' },
+    {id: 16, name: 'Wolverine' },
+    {id: 17, name: 'Storm' },
+    {id: 18, name: 'Gambit' },
+    {id: 19, name: 'Beast' }
+
+]
+
