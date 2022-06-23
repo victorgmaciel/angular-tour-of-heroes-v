7b0795ccc80f98983c3365875f356ed9e1bb9e90
c.victor.maclel <c.victor.maclel@z-mac-3466.local>
2022-06-23 13:25:05 -0700

    Completed Angular Tutorial - Tour of HEroes.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../heroes/hero';
-import { HEROES } from '../mock-heroes';
 
+import { Hero } from '../heroes/hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,21 +9,31 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
 
-
-  heroes = HEROES;
-
-  selectedHero?: Hero;
-  onSelect(hero: Hero):void {
-    this.selectedHero = hero;
-  }
-
-
-  constructor() { }
-
-  
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
 
 }
